342272b8bf730839b28a7393b7c80581837d83cb
MTJoker <bernd@Bernds-MBP.local>
2023-10-20 21:56:12 +0200

    day 10 part 1

--- 2022/day10/src/day10.cpp
+++ 2022/day10/src/day10.cpp
@@ -1,9 +1,77 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
-void main_part1()
+enum class Opcode { unknown, noop, addx };
+
+struct Command
 {
-    std::cout << "Day 10, part 1, answer is: " << 0 << std::endl;
+    Command()
+    : opcode(Opcode::unknown)
+    , param(0)
+    {}
+    Opcode opcode;
+    int param;
+};
+
+using Commands = std::vector<Command>;
+using Register = std::vector<int>;
+
+class CPU
+{
+public:
+
+    CPU()
+    : regX{1}
+    {}
+
+    void Excecute(Command& cmd)
+    {
+        switch(cmd.opcode)
+        {
+            case Opcode::noop:
+            {
+                regX.push_back(regX.back());
+                break;
+            }
+            case Opcode::addx:
+            {
+                regX.push_back(regX.back());
+                regX.push_back(regX.back() + cmd.param);
+                break;
+            }
+            default:
+            {
+                throw;
+            }
+        }
+    }
+
+    unsigned getX(unsigned cycle) { return regX[cycle]; }
+
+private:
+
+    Register regX;
+};
+
+void main_part1(Commands commands)
+{
+    CPU cpu;
+
+    for(auto& command : commands)
+    {
+        cpu.Excecute(command);
+    }
+
+    auto cycles = std::vector<unsigned>{20, 60, 100, 140, 180, 220};
+    auto signalStrength = 0;
+
+    for(auto cycle : cycles)
+    {
+        signalStrength += cycle * cpu.getX(cycle - 1);
+    }
+
+    std::cout << "Day 10, part 1, answer is: " << signalStrength << std::endl;
 }
 
 void main_part2()
@@ -17,8 +85,36 @@ int main(void)
 
     if(input.is_open())
     {
+        Commands commands;
+        std::string line;
+        
+        while (std::getline(input, line)) 
+        {
+            Command command;
 
-        main_part1();
+            std::istringstream ss(line);
+            std::string opcodeStr;
+            ss >> opcodeStr;
+
+            if (opcodeStr == "noop") 
+            {
+                command.opcode = Opcode::noop;
+            } 
+            else if (opcodeStr == "addx") 
+            {
+                command.opcode = Opcode::addx;
+            }
+
+            unsigned paramValue;
+            if (ss >> paramValue)
+            {
+                command.param = paramValue;
+            }
+
+            commands.push_back(command);
+        } 
+
+        main_part1(commands);
         main_part2();
     }
    
